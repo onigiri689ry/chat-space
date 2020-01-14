d95012c63c981406c5c10f4417f2986ca2f4f12e
onigiri689ry <59249071+onigiri689ry@users.noreply.github.com>
2020-01-15 00:36:24 +0900

    ルーティング訂正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
   root "users#index"
-  resources :users, only: [:index, :edit, :update]
+  resources :users, only: [:index, :edit, :update, :edit,]
   resources :groups, only: [:index, :new, :create, :edit, :update, :show] do
     resources :messages, only: [:index, :create, :show]
   end
