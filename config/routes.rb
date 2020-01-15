2e8df9e4af1f3d2c048f85d41bba3a83257d51c2
onigiri689ry <59249071+onigiri689ry@users.noreply.github.com>
2020-01-15 11:45:00 +0900

    ルートを一つにする、edit１つ消す

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
-  root "users#index"
-  resources :users, only: [:index, :edit, :update, :edit,]
+  resources :users, only: [:index, :edit, :update,]
   resources :groups, only: [:index, :new, :create, :edit, :update, :show] do
     resources :messages, only: [:index, :create, :show]
   end
