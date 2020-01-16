4f704e5ea282286accba3f3379719569e473e78f
onigiri689ry <59249071+onigiri689ry@users.noreply.github.com>
2020-01-16 18:32:03 +0900

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:onigiri689/chat-spase.git'
+set :repo_url,  'git@github.com:onigiri689/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
